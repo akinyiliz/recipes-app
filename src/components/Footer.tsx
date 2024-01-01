d483c89b7e9a37a0d4c8e3f5f44ce6d81dd07bae
akinyiliz <lizzieke84@gmail.com>
2024-01-01 16:13:09 +0300

    feat: Implement Footer Component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-black p-6 flex items-center justify-center">
+      <p className="text-white text-sm md:text-lg">
+        &copy;2024 RecipeHub. All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
