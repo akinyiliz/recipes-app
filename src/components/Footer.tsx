0f3d081f4883ebd6f80f74ac9239cbc403f28154
akinyiliz <lizzieke84@gmail.com>
2024-01-02 12:43:17 +0300

    style: Add mt-auto to push the footer at the bottom of container

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="bg-black p-6 flex items-center justify-center">
+    <footer className="mt-auto bg-black p-6 flex items-center justify-center">
       <p className="text-white text-sm md:text-lg">
         &copy;{year} RecipeHub. All Rights Reserved.
       </p>
