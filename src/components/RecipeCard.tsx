813ed5217cc77c3e4aa5b61c373e953ecf47334c
akinyiliz <lizzieke84@gmail.com>
2024-06-01 12:35:31 +0300

    chore: Add lazy loading for recipe images in RecipeCard

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -3,12 +3,21 @@ import { Recipe } from "../types/recipe";
 
 function RecipeCard({ id, title, image }: Recipe) {
   return (
-    <Link to={"/recipe/" + id} className="relative w-full h-full" key={id}>
+    <Link
+      to={"/recipe/" + id}
+      className="relative w-full h-full hover:scale-105 transition-all duration-400 ease-in-out"
+      key={id}
+    >
       <div className="bg-black/40 w-full h-full absolute rounded-[2rem] z-10"></div>
       <p className="absolute z-20 text-white bottom-8 left-0 right-0 text-lg text-center">
         {title}
       </p>
-      <img src={image} alt={title} className="rounded-[2rem] w-full h-full" />
+      <img
+        src={image}
+        alt={title}
+        className="rounded-[2rem] w-full h-full"
+        loading="lazy"
+      />
     </Link>
   );
 }
